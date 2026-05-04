9eb83b8b1718285397fc46eb5e03bc64d48453df
yolozyrt <yoanncrosnier2306@gmail.com>
2026-05-04 14:28:52 +0200

    limit mode classique

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const APP_NAME = "DBDle";
 export const DEFAULT_MODE = "classic";
-export const MAX_GUESSES = 6;
+export const MAX_GUESSES = 10;
 export const DAILY_ANCHOR_DATE = "2024-01-01";
 export const SUPABASE_CONFIG = {
   url: import.meta.env.VITE_SUPABASE_URL || "",
